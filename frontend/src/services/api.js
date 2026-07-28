b1ab7fa2d7d0ccaa84f5ed769f3e265420887cf6
ScepticFlare <scepticflare@gmail.com>
2026-07-28 12:49:15 +0530

    Fix backend URL typo

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://crm-backend-bfwr.onrender.comi"
+    baseURL: "https://crm-backend-bfwr.onrender.com"
 });
 
 // Automatically attach JWT token
